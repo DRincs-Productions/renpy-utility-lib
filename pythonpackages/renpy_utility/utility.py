3c52771e556b6a347bfef23edd8980a54192d34f
Exodia <67595890+DonRP@users.noreply.github.com>
2023-09-22 17:17:34 +0200

    get all all_characters

--- pythonpackages/renpy_utility/utility.py
+++ pythonpackages/renpy_utility/utility.py
@@ -1,8 +1,10 @@
 from typing import Optional
+import renpy.character as character
 
 __all__ = [
     "isNullOrEmpty",
     "IsNullOrWhiteSpace",
+    "all_characters",
 ]
 
 
@@ -20,3 +22,11 @@ def IsNullOrWhiteSpace(item: Optional[str]) -> bool:
 
 def os_path_join(a: str, b: str) -> str:
     return a + "/" + b
+
+
+def all_characters(store) -> list[character.ADVCharacter]:
+    my_list: list[character.ADVCharacter] = []
+    for item in store.__dict__():
+        if isinstance(item, character.ADVCharacter):
+            my_list.append(item)
+    return my_list
